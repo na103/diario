1b8921f88a363af7171ff474933c85227a0da890
Nicola Avanzi <118681516+na103@users.noreply.github.com>
2026-09-18 10:42:07 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+// Tiene l'app disponibile anche senza rete.
+// Strategia: prima la rete (così un aggiornamento arriva subito), la copia salvata se la rete manca.
+const CACHE = "diario-assistenza";
+const FILES = ["./", "./index.html", "./app.js", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./icon-maskable-512.png"];
+
+self.addEventListener("install", (e) => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(FILES)).catch(() => {}));
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(caches.keys().then((ks) => Promise.all(ks.filter((k) => k !== CACHE).map((k) => caches.delete(k)))).then(() => self.clients.claim()));
+});
+
+self.addEventListener("fetch", (e) => {
+  const req = e.request;
+  if (req.method !== "GET" || new URL(req.url).origin !== self.location.origin) return;
+  e.respondWith(
+    fetch(req)
+      .then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE).then((c) => c.put(req, copy)).catch(() => {});
+        return res;
+      })
+      .catch(() => caches.match(req).then((hit) => hit || caches.match("./index.html")))
+  );
+});
